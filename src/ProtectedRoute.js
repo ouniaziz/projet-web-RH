4020dc49f6cf970814c6ab055137d31cdc6426f1
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-02-17 20:32:44 +0100

    merged front and back

--- src/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute() {
-  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-  console.log("Auth Status:", isAuthenticated); // Debugging
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
-}
--- src/layouts/dashboard/components/OrdersOverview/index.js
+++
@@ -1,5 +0,0 @@
-import Card from "@mui/material/Card";
-function OrdersOverview() {
-  return <Card sx={{ height: "100%" }}></Card>;
-}
-export default OrdersOverview;
