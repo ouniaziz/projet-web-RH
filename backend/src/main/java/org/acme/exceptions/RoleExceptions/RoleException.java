427440cb9fd772b8801b18d2d9dace703282450b
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-02-23 10:44:16 +0100

    Replace redundant files with generic EntityException

--- backend/src/main/java/org/acme/exceptions/RoleExceptions/RoleException.java
+++
@@ -1,8 +0,0 @@
-package org.acme.exceptions.RoleExceptions;
-
-public class RoleException extends RuntimeException{
-    public int status;
-    public RoleException(String msg, int s){
-        super(msg); status=s;
-    }
-}
