3ec90cbcfcc705c3c60f0e11d47ffb64375b1439
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-03-09 16:23:40 +0100

    Added filter endpoint for PersonResource

--- backend/src/main/java/org/acme/repositories/PersonRepository.java
+++ backend/src/main/java/org/acme/repositories/PersonRepository.java
@@ -1,6 +1,8 @@
 package org.acme.repositories;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.acme.DTO.PersonStatusDTO;
@@ -17,4 +19,32 @@ public class PersonRepository implements PanacheRepositoryBase<Person, String>{
         return find("email", email).project(PersonStatusDTO.class).firstResultOptional();
     }
 
+    public List<Person> filterRecord(String sexe, int grad, int anciennete, int handicap, String actif){
+        Map<String, Object> params = new HashMap<>();
+
+        // Build the query dynamically
+        StringBuilder query = new StringBuilder("FROM Person p where 1 = 1"); // Base condition to simplify chaining
+
+        if (grad != -1) {
+            query.append(" and p.grad.id_g = :grad");
+            params.put("grad", grad);
+        }
+        if (sexe != null) {
+            query.append(" and p.sexe = :sexe");
+            params.put("sexe", sexe);
+        }
+        if (anciennete != -1) {
+            query.append(" and p.anciennete = :anciennete");
+            params.put("anciennete", anciennete);
+        }
+        if (actif!= null && (actif.equals("false") || actif.equals("true"))) {
+            query.append(" and p.actif = :actif");
+            params.put("actif", actif);
+        }
+        if (handicap != -1) {
+            query.append(" and exists(select 1 from p.handicaps h where h.handicap.id_hand = :handicap)");
+            params.put("handicap", handicap);
+        }
+        return find(query.toString(), params).list();
+    }
 }
--- backend/src/main/java/org/acme/resources/PersonResource.java
+++ backend/src/main/java/org/acme/resources/PersonResource.java
@@ -1,19 +1,16 @@
 package org.acme.resources;
 
-import java.util.concurrent.CompletionStage;
-
 import org.acme.DTO.ApiResponseDTO;
 import org.acme.DTO.PersonDTO;
-import org.acme.brevo.services.BrevoService;
 import org.acme.entities.Person;
 import org.acme.services.PersonService;
+import org.jboss.resteasy.reactive.RestQuery;
 
-import io.smallrye.common.annotation.Blocking;
-import io.smallrye.mutiny.Uni;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
@@ -33,13 +30,28 @@ import sendinblue.ApiException;
 
     TODO: Ask chatbot how to integrate realtime notification manager that's fired upon every modification made to the Person's data
 
- *  TODO:GET getPersonsByFilter, PUT modify person, POST add person, DELETE delete person
+ *  TODO:GET getPersonsByFilter
+ *
+ *  possible filters for specific people: grade, sexe, handicap, nature du travail, ancienneté, actif/ non actif
+ * 
+ *  TODO: DELETE delete person
  * 
  * replace java-jwt dependency with smallrye-jwt when necessary since it integrates well with quarkus.
 */
 public class PersonResource {
     
     @Inject PersonService personService;
+
+    @GET
+    //@RolesAllowed({"Personnel RH", "Administrator"})
+    public Response getPersonsByFilters(@DefaultValue("-1")   @RestQuery int grad,
+                                        @RestQuery String sexe,
+                                        @DefaultValue("-1")   @RestQuery int anciennete,
+                                        @RestQuery String actif,
+                                        @DefaultValue("-1")   @RestQuery int handicap){
+        
+        return Response.ok(new ApiResponseDTO(200, "Filtered successfully", null, personService.filterRecords(sexe,grad,anciennete,handicap, actif))).build();
+    }
 
     @POST
     @Transactional
@@ -63,7 +75,6 @@ public class PersonResource {
     }
 
     @GET
-    
     @Path("/enseignant")
     //@RolesAllowed({"Personnel RH", "Administrator"})
     public Response getEnseignants(){
@@ -86,5 +97,10 @@ public class PersonResource {
     public Response modifyPerson(PersonDTO personDto, @Context SecurityContext ctx){
         personService.modifyPerson(personDto, ctx);
         return Response.status(200).entity(new ApiResponseDTO(200, "Modified person "+personDto.cin.get()+" successfully", null, null)).build();
+    }
+    @GET
+    @Path("all")
+    public Response getP(){
+        return Response.ok(personService.getPersons()).build();
     }
 }
