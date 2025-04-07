b20c5c9fb05c210a13e51a68dcfcc4160d7275b0
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-07 16:13:10 +0100

    removed unnecessary imports

--- backend/src/main/java/org/acme/exceptions/ActivationFailedExceptions/ActivationFailedException.java
+++ backend/src/main/java/org/acme/exceptions/ActivationFailedExceptions/ActivationFailedException.java
@@ -1,11 +1,5 @@
 package org.acme.exceptions.ActivationFailedExceptions;
 
-import org.acme.DTO.ApiResponseDTO;
-
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
-
 public class ActivationFailedException extends RuntimeException{
     int status;
     public ActivationFailedException(String msg, int status) {
