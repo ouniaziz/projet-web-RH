19b6446a0cf51985ab0bcc4c33b9ca3a30ef47da
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-07 17:01:17 +0100

    Add SoldeConge record upon creating Person record Uses PersonMapper interface instead of the deleted implementation of PersonMapperInt returns list of SimplePersonResponseDTO when fetching records for DataGrid visualization

--- backend/src/main/java/org/acme/services/PersonService.java
+++ backend/src/main/java/org/acme/services/PersonService.java
@@ -1,24 +1,30 @@
 package org.acme.services;
 
+import java.time.LocalDate;
+import java.time.Year;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
-import org.acme.DTO.PersonDTO;
+import org.acme.dto.PersonDTO;
 import org.acme.brevo.entities.BrevoAccountActivationTemplate;
 import org.acme.brevo.services.BrevoService;
-import org.acme.entities.GradEns;
-import org.acme.entities.HandicapPerson;
-import org.acme.entities.HandicapPersonId;
+import org.acme.dto.response.SimplePersonResponseDTO;
+import org.acme.entities.grad.Grad;
 import org.acme.entities.Person;
 import org.acme.entities.RolePerson;
 import org.acme.entities.User;
+import org.acme.entities.conge.SoldeConge;
+import org.acme.entities.conge.SoldeCongeId;
+import org.acme.entities.conge.TypeConge;
+import org.acme.entities.grad.GradPerson;
+import org.acme.entities.grad.GradPersonId;
+import org.acme.entities.handicap.HandicapPerson;
+import org.acme.entities.handicap.HandicapPersonId;
 import org.acme.exceptions.EntityException.EntityException;
-import org.acme.mappers.PersonMapper;
-import org.acme.repositories.PersonRepository;
-import org.acme.repositories.RolesRepository;
-import org.acme.repositories.UserRepository;
+import org.acme.interfaces.PersonMapper;
+import org.acme.repositories.*;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -31,22 +37,36 @@ public class PersonService {
     
     @Inject PersonRepository personRepository;
     @Inject RolesRepository rolesRepository;
-    @Inject BrevoService brevoService;
+    @Inject HandicapRepository handicapRepository;
     @Inject UserRepository userRepository;
+
+    @Inject BrevoService brevoService;
     @Inject JwtService jwtService;
-    @Inject PersonMapper personMapper;
-    
+    @Inject
+    PersonMapper personMapper;
+    @Inject
+    GradPersonRepository gradPerson;
+    @Inject
+    GradPersonRepository gradPersonRepository;
+
     // for now, I'll add activation token to the ApiResponseDTO
     public String addPerson(PersonDTO personDTO)throws ApiException{
         if(personRepository.findByIdOptional(personDTO.cin.get()).isPresent())
             throw new EntityException("Person with CIN = "+personDTO.cin.get()+ " already exists", 409);
         
         // Save person record
-        Person person = personMapper.toEntity(personDTO);
-        //personRepository.persist(person);
+        Person person = personMapper.toNewEntity(personDTO, rolesRepository, handicapRepository);
+        person.setStatus_p(Person.STATUS_PERSON_INACTIVE);
+        personRepository.persist(person);
+
+        // This adds SoldeConge
+        var soldeInitial =  TypeConge.<TypeConge>findByIdOptional(TypeConge.ID_CONGE_ANNUELLE).orElseThrow(()-> new EntityException("Type conge annuelle not found", 404)).getSold_initial();
+        SoldeConge solde = new SoldeConge(new SoldeCongeId(Year.now().getValue(), person.getCin()), soldeInitial);
+        person.getSoldeList().add(solde);
 
         // generate password activation token
         String activationToken = jwtService.generateActivationToken(person.getCin(), RolePerson.ROLES[personDTO.roleId.get().intValue()]);
+
         // Save user record corresponding to the person
         userRepository.persist(new User(personDTO.cin.get(), personDTO.email.get(), PasswordUtils.hashPassword(activationToken)));
 
@@ -55,7 +75,7 @@ public class PersonService {
         return activationToken;
     }
 
-    public void modifyPerson(PersonDTO personDTO, SecurityContext ctx){
+    public void modifyPerson(PersonDTO personDTO, SecurityContext ctx) {
         
         if(!ctx.getUserPrincipal().getName().equals(personDTO.cin.get()) && (!jwtService.getAuthRoles().contains(RolePerson.ADMIN_NAME) || !jwtService.getAuthRoles().contains(RolePerson.RH_NAME)))
             throw new EntityException("You can't modify other people's credentials", 401);
@@ -67,8 +87,31 @@ public class PersonService {
         personDTO.sexe.ifPresent(person::setSexe);
         personDTO.dateN.ifPresent(person::setDate_n);
 
-        personMapper.updateComplexAttributesFromDto(person, personDTO);
-    }
+        personDTO.roleId.ifPresent(roleId->{
+            RolePerson role = rolesRepository.findByIdOptional(roleId).orElseThrow(()-> new EntityException("role id="+roleId+" not found", 404));
+            person.setRole(role);
+        });
+
+        personDTO.gradId.ifPresent(gradId->{
+            List<GradPerson> gradList = person.getGradList();
+            gradList.get(0).setEndDate(LocalDate.now());
+            Grad.findByIdOptional(gradId).orElseThrow(()-> new EntityException("grad id="+gradId+" not found", 404));
+            gradList.add(
+                    new GradPerson(new GradPersonId(gradId,personDTO.cin.get()))
+            );
+        });
+
+        personDTO.handicaps.ifPresent(handicapList->{
+            handicapList.forEach(handicapDto->{
+                var handicap = handicapRepository.findByIdOptional((long)handicapDto.id).orElseThrow(()-> new EntityException("Handicap id="+handicapDto.id+" not found", 404));
+                var hp = new HandicapPerson();
+                hp.setId(new HandicapPersonId((long)handicapDto.id, personDTO.cin.get()));
+                hp.setSeverity(handicapDto.severity);
+                hp.setAssistive_devices(handicapDto.assistiveDevice);
+                hp.setHandicap(handicap);
+                hp.setPerson(person);
+            });
+        });    }
 
     public void archivePerson(String cin){
         Person p = personRepository.findByIdOptional(cin).orElseThrow(()-> new EntityException("Person id="+cin+" not found", 404));
@@ -112,21 +155,16 @@ public class PersonService {
         }
 
         String finalQuery = isHandicapJoin? "SELECT DISTINCT p "+query.toString():query.toString();
-        /*
-        * SELECT DISTINCT p
-        * FROM Person p, p.handicaps h
-        * WHERE p.grad = grad AND p.sexe = sexe AND p.anciennete = anciennete AND p.actif = actif
-        * AND h.handicap.id_hand = handicap
-        * */
         return personRepository.find(finalQuery, params).list();
     }
 
-    public List<Person> getEmployers(){
-        return personRepository.list("role.id_r IN (?1,?2,?3)", RolePerson.EMPLOYE_ID, RolePerson.ADMIN_ID, RolePerson.RH_ID);
+    // TODO: test this as I doubt it'll work, oh and See the performance implications
+    public List<SimplePersonResponseDTO> getEmployers(){
+        return personRepository.findByRoles(RolePerson.EMPLOYE_ID, RolePerson.ADMIN_ID, RolePerson.RH_ID);
     }
 
-    public List<Person> getEnseignant(){
-        return personRepository.list("role.id_r", RolePerson.ENSEIGNANT_ID);
+    public List<SimplePersonResponseDTO> getEnseignant(){
+        return personRepository.findByRoles(RolePerson.ENSEIGNANT_ID);
     }
 
     public List<Person> getPersons(){
