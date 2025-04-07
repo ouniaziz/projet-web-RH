5d8c505be1462dcc560b6c6aede166d0d41db457
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-07 16:58:03 +0100

    Added findByRoles to improve performance while converting Person records to SimplePersonResponseDTO

--- backend/src/main/java/org/acme/repositories/PersonRepository.java
+++ backend/src/main/java/org/acme/repositories/PersonRepository.java
@@ -1,11 +1,10 @@
 package org.acme.repositories;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import org.acme.DTO.PersonStatusDTO;
+import org.acme.dto.PersonStatusDTO;
+import org.acme.dto.response.SimplePersonResponseDTO;
 import org.acme.entities.Person;
 
 
@@ -19,4 +18,22 @@ public class PersonRepository implements PanacheRepositoryBase<Person, String>{
         return find("email", email).project(PersonStatusDTO.class).firstResultOptional();
     }
 
+    public List<SimplePersonResponseDTO> findByRoles(int... roles){
+        //TOD0: try NEW SimplePersonResponse, without package
+        return find("SELECT p.cin, p.nom, p.prenom, p.sexe, p.dateN, p.status_p, p.anciennete, p.email, encode(p.image, 'base64') as image, r.nom_r role,"+
+                            "(SELECT g.nom_h" +
+                            "FROM GradPerson gp, Grad g" +
+                            "WHERE gp.gradId = g.grad_id AND gp.id.cin=p.cin" +
+                            "ORDER BY start DESC" +
+                            "LIMIT 1) grad, "+
+                        "EXISTS(" +
+                            "SELECT 1" +
+                            "FROM p.handicaps hp" +
+                            "WHERE hp.id.cin = p.cin" +
+                            "LIMIT 1) as hasHandicaps"+
+                        "BOOL_OR(hp.id.HANDICAP_ID IS NOT NULL)" +
+                        "FROM Person" +
+                        "JOIN RolePerson r" +
+                        "WHERE r.id_r = ANY(?1)", List.of(roles)).project(SimplePersonResponseDTO.class).list();
+    }
 }
