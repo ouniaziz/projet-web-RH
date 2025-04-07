5fbc7e41c1766f0747708b89425143798c037787
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-07 16:51:30 +0100

    Added repository for DemandeConge

--- backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
+++ backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
@@ -1,11 +1,9 @@
 package org.acme.repositories;
 
-import org.acme.entities.GradEns;
-
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.entities.conge.DemandeConge;
 
 @ApplicationScoped
-public class GradRepository implements PanacheRepository<GradEns> {
-    
+public class DemandeCongeRepository implements PanacheRepository<DemandeConge> {
 }
