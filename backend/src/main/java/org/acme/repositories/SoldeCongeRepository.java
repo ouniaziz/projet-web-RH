77dbe6e2713acc7724f86421fb0e11c6c15bc2cf
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:22:37 +0100

    just a little mundane modification

--- backend/src/main/java/org/acme/repositories/SoldeCongeRepository.java
+++ backend/src/main/java/org/acme/repositories/SoldeCongeRepository.java
@@ -10,8 +10,11 @@ import java.util.List;
 
 @ApplicationScoped
 public class SoldeCongeRepository implements PanacheRepositoryBase<SoldeConge, SoldeCongeId> {
-    public List<SoldeConge> findByCin(String cin){
-        var current_year =Year.now().getValue();
-        return find("id.cin = ?1 AND id.annee BETWEEN ?2 AND ?3 AND solde_restant>0 ORDER BY id.annee ASC", cin, current_year, current_year - SoldeConge.PLAFOND_ANNEE).list();
+    public List<SoldeConge> findByCin(String cin, int demande_annee){
+        return find("""
+                    SELECT s
+                    FROM SoldeConge s
+                    WHERE s.id.cin = ?1 AND s.id.annee BETWEEN ?2 AND ?3 AND s.soldeRestant>0
+                    ORDER BY id.annee ASC""", cin,  demande_annee - SoldeConge.PLAFOND_ANNEE, demande_annee).list();
     }
 }
