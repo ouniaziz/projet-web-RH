487837d4d12387e04044124f936fb8bd7745d8e8
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:12:24 +0100

    Added associations

--- backend/src/main/java/org/acme/entities/Exercice.java
+++ backend/src/main/java/org/acme/entities/Exercice.java
@@ -3,14 +3,30 @@ package org.acme.entities;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import org.acme.entities.conge.Conge;
+import org.acme.entities.conge.DemandeConge;
+import org.acme.entities.conge.SoldeConge;
+
+import java.util.List;
 
 @Entity
 public class Exercice extends PanacheEntityBase {
     @Id
     private int annee;
 
+    @OneToMany(mappedBy = "exercice")
+    private List<Conge> congeList;
+
+    @OneToMany(mappedBy = "exercice")
+    private List<SoldeConge> soldeCongeList;
+
+    @OneToMany(mappedBy = "exercice")
+    private List<DemandeConge> demandeList;
+
     public Exercice() {}
     public int getAnnee() {
         return annee;
     }
+    public void setAnnee(int an){this.annee=an;}
 }
