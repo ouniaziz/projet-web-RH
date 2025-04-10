10a25a12017474a8021fea20893e4262a0433e10
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:03:26 +0100

    Removed annee, status and id because they aren't needed

--- backend/src/main/java/org/acme/dto/conge/DemandeCongeDTO.java
+++ backend/src/main/java/org/acme/dto/conge/DemandeCongeDTO.java
@@ -3,17 +3,13 @@ package org.acme.dto.conge;
 import java.time.LocalDate;
 
 public class DemandeCongeDTO {
-    public Long id;
     public LocalDate dateDebut;
     public LocalDate dateFin;
     public LocalDate dateRetour;
     public int duree;
 
-    public int statusConge;
-
     public String cin;
 
-    public int annee;
-
     public int type_id;
+
 }
