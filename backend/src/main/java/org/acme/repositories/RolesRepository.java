887907c7b356f05ee6e39725590a20dd6118b461
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:20:48 +0100

    Slight modification

--- backend/src/main/java/org/acme/repositories/RolesRepository.java
+++ backend/src/main/java/org/acme/repositories/RolesRepository.java
@@ -1,11 +1,12 @@
 package org.acme.repositories;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import org.acme.entities.RolePerson;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class RolesRepository implements PanacheRepository<RolePerson>{
+public class RolesRepository implements PanacheRepositoryBase<RolePerson,Long> {
     
 }
