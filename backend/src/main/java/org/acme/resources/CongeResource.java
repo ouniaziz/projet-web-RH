c524c26f2cd9acf69735068c5f72bff0db187082
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 20:50:45 +0100

    Added some TODOS and @GET List<DemandeConges>

--- backend/src/main/java/org/acme/resources/CongeResource.java
+++ backend/src/main/java/org/acme/resources/CongeResource.java
@@ -9,12 +9,14 @@ import org.acme.dto.conge.TypeCongeDTO;
 import org.acme.services.CongeService;
 
 @Path("/api/conges")
+// TODO: Test the resource and Conge operations and Add @GET Demande & @GET Congé
+// TODO: Now, Add method to import already existing congés into the DB. Don't get here until you tested evverything prior to that
 public class CongeResource {
     @Inject
     CongeService congeService;
 
     @GET
-    @Path("/types")
+    @Path("/type")
     public Response getTypesConge(){
         return Response.ok(new ApiResponseDTO(200,"Fetched Types conge successfully", null,congeService.getTypesConges())).build();
     }
@@ -26,11 +28,17 @@ public class CongeResource {
         return Response.accepted(new ApiResponseDTO(201, "Type congé created successfully", null, null)).build();
     }
 
+    @GET
+    @Path("/demande")
+    public Response getDemandes(){
+        return Response.ok(new ApiResponseDTO(200, "Fetched demandes de congés", null, congeService.getDemandes())).build();
+    }
+
     @POST
     @Path("/demande")
     public Response addDemande(DemandeCongeDTO demande){
-        congeService.createDemande(demande);
-        return Response.accepted(new ApiResponseDTO(201, "Demande congé created successfully", null, null)).build();
+        Long id = congeService.createDemande(demande);
+        return Response.accepted(new ApiResponseDTO(201, "Demande congé id="+id+" created successfully", null, null)).build();
     }
 
     @PUT
