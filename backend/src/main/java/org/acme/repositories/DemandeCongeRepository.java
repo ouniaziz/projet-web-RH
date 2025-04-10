a954462b379cbe287604bfb1b32b87c7e483eff8
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:12:12 +0100

    Added something similar to findById, but optimized by not fetching unnecessary entities, thus avoiding redundant select queries

--- backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
+++ backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
@@ -4,6 +4,18 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.acme.entities.conge.DemandeConge;
 
+import java.util.Optional;
+
 @ApplicationScoped
 public class DemandeCongeRepository implements PanacheRepository<DemandeConge> {
+    public Optional<DemandeConge> findByIdOptimized(Long id){
+        return find("""
+                SELECT d
+                FROM DemandeConge d
+                LEFT JOIN FETCH d.person
+                LEFT JOIN FETCH d.exercice
+                LEFT JOIN FETCH d.type
+                WHERE d.id= ?1
+                """, id).singleResultOptional();
+    }
 }
