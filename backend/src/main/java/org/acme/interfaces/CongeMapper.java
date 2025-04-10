537d3ef9fe8e15f1fb8bc78a7e194b9c9d0b8062
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 20:49:39 +0100

    Added additional imports Removed unnecessary mapping to statusConge, because every demandeConge will be PENDING by default

--- backend/src/main/java/org/acme/interfaces/CongeMapper.java
+++ backend/src/main/java/org/acme/interfaces/CongeMapper.java
@@ -3,6 +3,7 @@ package org.acme.interfaces;
 import org.acme.dto.conge.DemandeCongeDTO;
 import org.acme.entities.Exercice;
 import org.acme.entities.Person;
+import org.acme.entities.conge.Conge;
 import org.acme.entities.conge.DemandeConge;
 import org.acme.entities.conge.TypeConge;
 import org.acme.exceptions.EntityException.EntityException;
@@ -11,15 +12,17 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "cdi")
+import java.time.Year;
+
+@Mapper(componentModel = "cdi", imports = {Year.class})
 public interface CongeMapper {
     @Mapping(target="dateDebut", source = "dto.dateDebut")
     @Mapping(target="dateFin", source = "dto.dateFin")
     @Mapping(target="dateRetour", source = "dto.dateRetour")
     @Mapping(target="duree", source = "dto.duree")
-    @Mapping(target="statusConge", source = "dto.statusConge")
+    @Mapping(target="statusConge", ignore = true)
     @Mapping(target="person", expression = "java(mapPerson(dto.cin, personRepo))")
-    @Mapping(target="exercice", expression = "java(mapExercice(dto.annee))")
+    @Mapping(target="exercice", expression = "java(mapExercice(Year.now().getValue()))")
     @Mapping(target="type", expression = "java(mapType(dto.type_id))")
     DemandeConge dtoToDemande(DemandeCongeDTO dto, @Context PersonRepository personRepo);
 
