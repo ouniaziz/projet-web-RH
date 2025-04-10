0f82be70c3aee10146567c5385ad17323c264ab4
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-10 21:21:10 +0100

    Turned this bottleneck of a DTO into a simple record

--- backend/src/main/java/org/acme/dto/response/SimplePersonResponseDTO.java
+++ backend/src/main/java/org/acme/dto/response/SimplePersonResponseDTO.java
@@ -1,36 +1,10 @@
 package org.acme.dto.response;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.acme.entities.grad.GradPerson;
 
 import java.time.LocalDate;
-import java.util.List;
 
-public class SimplePersonResponseDTO {
-    public String cin;
-    public String nom;
-    public String prenom;
-    public String sexe;
-    public LocalDate dateN;
-    public int status;
-    public int anciennete;
-    public String email;
-    public String image;
-    public String role;
-    public String grad; // current grad
-    public boolean hasHandicap;
-
-    public SimplePersonResponseDTO(String cin, String nom, String prenom, String sexe, LocalDate dateN, int status, int anciennete, String email, String image, String role, String grad, boolean hasHandicap) {
-        this.cin = cin;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.sexe = sexe;
-        this.dateN = dateN;
-        this.status = status;
-        this.anciennete = anciennete;
-        this.email = email;
-        this.image = image;
-        this.role = role;
-        this.grad = grad;
-        this.hasHandicap = hasHandicap;
-    }
-}
+public record SimplePersonResponseDTO (String cin, String nom, String prenom, String sexe,
+                                       Integer status, LocalDate dateN, String email, Integer anciennete, String role,
+                                       String grad, Boolean hasHandicaps, String image){}
