f40b12316664bd7d675712e46ae61196f1ffdcf5
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-11 08:30:48 +0100

    From record to regular class I only changed it to add little condition on image attribute

--- backend/src/main/java/org/acme/dto/response/SimplePersonResponseDTO.java
+++ backend/src/main/java/org/acme/dto/response/SimplePersonResponseDTO.java
@@ -4,7 +4,34 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.acme.entities.grad.GradPerson;
 
 import java.time.LocalDate;
+import java.util.Base64;
 
-public record SimplePersonResponseDTO (String cin, String nom, String prenom, String sexe,
-                                       Integer status, LocalDate dateN, String email, Integer anciennete, String role,
-                                       String grad, Boolean hasHandicaps, String image){}
+public class SimplePersonResponseDTO {
+    public final String cin;
+    public final String nom;
+    public final String prenom;
+    public final String sexe;
+    public final Integer status;
+    public final LocalDate dateN;
+    public final String email;
+    public final Integer anciennete;
+    public final String role;
+    public final String grad;
+    public final Boolean hasHandicaps;
+    public final String image;
+
+    public SimplePersonResponseDTO(String cin, String nom, String prenom, String sexe, Integer status, LocalDate dateN, String email, Integer anciennete, String role, String grad, Boolean hasHandicaps, byte[] image) {
+        this.cin = cin;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.sexe = sexe;
+        this.status = status;
+        this.dateN = dateN;
+        this.email = email;
+        this.anciennete = anciennete;
+        this.role = role;
+        this.grad = grad;
+        this.hasHandicaps = hasHandicaps;
+        this.image = image!=null?Base64.getEncoder().encodeToString(image):"No image";
+    }
+}
