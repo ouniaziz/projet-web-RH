d5511dc9c95df7ea1efb0d5ea4c1dabc32635969
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-04-18 11:25:56 +0100

    Added ParamResource.java Laid the groundwork for such, with TODOs!

---
+++ backend/src/main/java/org/acme/resources/ParamResource.java
@@ -0,0 +1,10 @@
+package org.acme.resources;
+
+import jakarta.ws.rs.Path;
+
+//@Path("/api/params")
+/*  TODO: ADD @GET to params: RolePerson, CRUD to Departement, Grad and Handicaps
+    TODO: make handicaps dynamic, if there's no handicap, it's created on the spot!
+ */
+public class ParamResource {
+}
