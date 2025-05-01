30afefe8d6d18693a113045cf29be994bba4b4c5
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-01 21:03:58 +0100

    Added NotificationProvider.jsx This provider displays notifications, following notistack implementation. It communicates with notificationService.js to ease the pain

---
+++ frontend/src/components/NotificationProvider.jsx
@@ -0,0 +1,36 @@
+import { useNotificationStore } from '../service/notificationService';
+import MDSnackbar from 'components/MDSnackbar';
+import PropTypes from "prop-types";
+import { SnackbarProvider } from 'notistack';
+import {forwardRef} from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import {Alert} from "@mui/material";
+import Fade from "@mui/material/Fade";
+import IconButton from "@mui/material/IconButton";
+import Icon from "@mui/material/Icon";
+import Snackbar from "@mui/material/Snackbar";
+import {CustomNotification} from "./CustomComponents";
+
+
+
+// eslint-disable-next-line react/prop-types
+export const NotistackProvider = ({ children }) => {
+    const setupSnackbar  = useNotificationStore((state)=>state.setupSnackbar);
+    return (
+        <SnackbarProvider
+            maxSnack={3}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            ref={(provider) => {
+                if (provider) {
+                    setupSnackbar(provider.enqueueSnackbar, provider.closeSnackbar);
+                }
+            }}
+            Components={{
+                    success: CustomNotification,
+                    error: CustomNotification
+            }}
+        >
+            {children}
+        </SnackbarProvider>
+    );
+};
