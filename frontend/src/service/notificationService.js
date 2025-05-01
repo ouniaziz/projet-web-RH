0883446dc3b6d6c31a9ce86236fec18b51e5a127
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-01 21:05:17 +0100

    Significantly improved store Now store uses both: notification list and notistack's notification system The former is for navbar bell icon The latter is for NotificationProvider, but I set maximum notification on screen to be 3

--- frontend/src/service/notificationService.js
+++ frontend/src/service/notificationService.js
@@ -1,34 +1,50 @@
 import { create } from 'zustand';
+import notifications from "../layouts/notifications";
+import {useSnackbar} from "notistack";
 
-export const useNotificationStore = create((set) => ({
+export const useNotificationStore = create((set, get) => ({
     notifications: [],
+    enqueueSnackbar: null,
+    closeNotification: null,
+    // Initialize notistack
+    setupSnackbar: (enqueueFn, closeNotif) => set({ enqueueSnackbar: enqueueFn, closeNotification:closeNotif }),
 
-    // Show a new notification
+    // Wrapper for showing notifications
+    /* This is the PropTypes of notifications
+    * notification:
+    ** type: 'error' | "info" | "success",
+    ** content: string,
+    ** title: string,
+    **/
     showNotification: (notification) => {
-        const id = Date.now();
-        set((state) => ({
-            notifications: [...state.notifications, { ...notification, id, open: true }],
-        }));
+        const { enqueueSnackbar, notifications } = useNotificationStore.getState();
+        set({
+            notifications: [...notifications, notification]
+        });
 
-       /* // Auto-close after 5 seconds
-        setTimeout(() => {
-            useNotificationStore.getState().closeNotification(id);
-        }, 5000);*/
+        if (enqueueSnackbar) {
+            enqueueSnackbar(notification.content, {
+                variant: notification.type || 'default',
+                autoHideDuration: null, // Persistent until clicked
+                anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                },
+                title: notification.title
+            });
+        }
     },
 
-    // Close a notification by ID
-    closeNotification: (id) => {
-        set((state) => ({
-            notifications: state.notifications.map((notif) =>
-                notif.id === id ? { ...notif, open: false } : notif
-            ),
-        }));
-
-        /*// Remove from state after fade-out
-        setTimeout(() => {
-            set((state) => ({
-                notifications: state.notifications.filter((notif) => notif.id !== id),
-            }));
-        }, 300);*/
+    removeNotification: (index)=>{
+        const {notifications} = useNotificationStore.getState()
+        set({
+            notifications: notifications.filter((_, i) => i!==index)
+        })
     },
+
+    clearAll: ()=>{
+        set({
+            notifications: []
+        })
+    }
 }));
