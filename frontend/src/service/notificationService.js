3fced612137baa4285b31f72735d4f2cc742dea6
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-02 11:21:33 +0100

    Added showFloatingNotificaiton Seperate notification method to manage only floating notifications Now auth notifications won't show in the navbar

--- frontend/src/service/notificationService.js
+++ frontend/src/service/notificationService.js
@@ -35,6 +35,28 @@ export const useNotificationStore = create((set, get) => ({
         }
     },
 
+    /* This is the PropTypes of notifications
+    * notification:
+    ** type: 'error' | "info" | "success",
+    ** content: string,
+    ** title: string,
+    **/
+    showFloatingNotification: (notification) => {
+        const { enqueueSnackbar} = useNotificationStore.getState();
+
+        if (enqueueSnackbar) {
+            enqueueSnackbar(notification.content, {
+                variant: notification.type || 'default',
+                autoHideDuration: null, // Persistent until clicked
+                anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                },
+                title: notification.title
+            });
+        }
+    },
+
     removeNotification: (index)=>{
         const {notifications} = useNotificationStore.getState()
         set({
