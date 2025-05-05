32390fe5e70cf2d004e9e448293d182fed126473
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-05 06:25:54 +0100

    modified file for readability

--- backend/src/main/java/org/acme/dto/response/CongeDTO.java
+++ backend/src/main/java/org/acme/dto/response/CongeDTO.java
@@ -2,7 +2,12 @@ package org.acme.dto.response;
 
 import java.time.LocalDate;
 
-public record CongeDTO(Integer id, LocalDate dateDebut, LocalDate dateFin, LocalDate dateRetour,
-                       Integer duree, Integer annee, String type){
+public record CongeDTO(Integer id,
+                       LocalDate dateDebut,
+                       LocalDate dateFin,
+                       LocalDate dateRetour,
+                       Integer duree,
+                       Integer annee,
+                       String type){
 
 }
