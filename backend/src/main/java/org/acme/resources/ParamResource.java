056887bb5e1640eb60cf0ee3062c56cdb851bc19
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-05 06:35:42 +0100

    changed nothing important

--- backend/src/main/java/org/acme/resources/ParamResource.java
+++ backend/src/main/java/org/acme/resources/ParamResource.java
@@ -7,11 +7,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import org.acme.dto.response.ApiResponseDTO;
 import org.acme.entities.Department;
+import org.acme.entities.conge.JoursFeriers;
 import org.acme.entities.grad.Grad;
 import org.acme.entities.handicap.Handicap;
 
 /*  TODO: ADD @GET to params: RolePerson, CRUD to Departement, Grad and Handicaps
     TODO: make handicaps dynamic, if there's no handicap, it's created on the spot!
+    TODO: CRUD JoursFeriers
  */
 @Path("/api/params")
 public class ParamResource {
@@ -19,30 +21,30 @@ public class ParamResource {
     @Path("/grad")
     @GET
     //RolesAllowed(**)
-    public Response getGrads(){
-        return Response.ok(new ApiResponseDTO(200,"Fetched Grads", null, Grad.listAll())).build();
+    public Response getGrads() {
+        return Response.ok(new ApiResponseDTO(200, "Fetched Grads", null, Grad.listAll())).build();
     }
 
     @Path("/handicap")
     @GET
     //RolesAllowed(**)
-    public Response getHandicaps(){
-        return Response.ok(new ApiResponseDTO(200,"Handicappe fetched", null, Handicap.listAll())).build();
+    public Response getHandicaps() {
+        return Response.ok(new ApiResponseDTO(200, "Handicappe fetched", null, Handicap.listAll())).build();
     }
 
     @Path("/handicap")
     @POST
     @Transactional
-    public Response addHandicap(Handicap handicap){
+    public Response addHandicap(Handicap handicap) {
         Handicap.persist(handicap);
-        return Response.ok(new ApiResponseDTO(200,"Handicap id="+handicap.getId_hand()+" added successfully", null, null)).build();
+        return Response.ok(new ApiResponseDTO(200, "Handicap id=" + handicap.getId_hand() + " added successfully", null, null)).build();
     }
 
     @Path("/departement")
     @GET
     //RolesAllowed(**)
-    public Response getDepartement(){
-        return Response.ok(new ApiResponseDTO(200,"Departements fetched", null, Department.list("SELECT d FROM Department d"))).build();
+    public Response getDepartement() {
+        return Response.ok(new ApiResponseDTO(200, "Departements fetched", null, Department.list("SELECT d FROM Department d"))).build();
     }
 
 }
