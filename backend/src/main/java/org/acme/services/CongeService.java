324aef0ac9dfbd8b6c1417cc0c1a43a7ebcecc63
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-05 06:28:08 +0100

    Added methods for following endpoints Jours feriers (@GET and @POST) dateFinRetour (@POST) Also made a better implementation for acceptConge method

--- backend/src/main/java/org/acme/services/CongeService.java
+++ backend/src/main/java/org/acme/services/CongeService.java
@@ -5,27 +5,29 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.SecurityContext;
-import org.acme.dto.conge.DemandeAjoutSoldeDTO;
-import org.acme.dto.conge.DemandeCongeDTO;
-import org.acme.dto.conge.TypeCongeDTO;
+import org.acme.dto.conge.*;
 import org.acme.dto.response.CongeDTO;
 import org.acme.entities.Notification;
 import org.acme.entities.RolePerson;
-import org.acme.entities.conge.Conge;
-import org.acme.entities.conge.DemandeConge;
-import org.acme.entities.conge.SoldeConge;
-import org.acme.entities.conge.TypeConge;
+import org.acme.entities.conge.*;
 import org.acme.exceptions.EntityException.EntityException;
 import org.acme.interfaces.CongeMapper;
-import org.acme.interfaces.PersonMapper;
 import org.acme.repositories.DemandeCongeRepository;
 import org.acme.repositories.PersonRepository;
 import org.acme.repositories.SoldeCongeRepository;
 import org.jboss.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 //TODO: test notifying the user
@@ -44,11 +46,12 @@ public class CongeService {
         personRepository.existsOrElseThrow(dto.cin);
         var demandeConge = congeMapper.dtoToDemande(dto, personRepository);
         demandeConge.setStatusConge(DemandeConge.DEMANDE_PENDING);
-        demandeCongeRepository.persist(demandeConge);
+
         // Notify Admin
         Notification notif = new Notification("Demande congé reçu",dto.cin+" requested a congé", "info", dto.cin);
         notificationService.sendToRole(RolePerson.ADMIN_ID, notif);
 
+        demandeCongeRepository.persist(demandeConge);
         return demandeConge.id;
     }
 
@@ -76,18 +79,48 @@ public class CongeService {
         notificationService.sendMsg(notif);
     }
 
+    private short updateBalance(List<SoldeConge> soldeListe, short duree, boolean useSoldeRestant){
+        var iterator = soldeListe.listIterator();
+
+        while (iterator.hasNext() && duree != 0) {
+            SoldeConge element = iterator.next();
+
+            // Choose the getter & setter dynamically
+            Supplier<Integer> getter = useSoldeRestant
+                    ? element::getSoldeRestant
+                    : element::getSoldeCompRestant;
+
+            Consumer<Integer> setter = useSoldeRestant
+                    ? element::setSoldeRestant
+                    : element::setSoldeCompRestant;
+
+            Integer currentSolde = getter.get();
+
+            if (duree - currentSolde > 0) {
+                duree -= currentSolde;
+                setter.accept(0);
+            } else {
+                setter.accept(currentSolde - duree);
+                duree = 0;
+            }
+        }
+
+        return duree;
+    }
+
     @Transactional
+    //TODO: Add an upper limit to number of days for leaving work
     public void acceptConge(Long demande_id){
         DemandeConge demande = demandeCongeRepository.findByIdOptional(demande_id).orElseThrow(()->new EntityException("Demande id="+demande_id+" not found", 404));
         if(demande.getStatusConge()!= DemandeConge.DEMANDE_PENDING)
             throw new EntityException("Demande id="+demande_id+" already decided",400);
 
         // First check if date attributes are valid
-        var date_deb = demande.getDateDebut();
-        var date_retour = demande.getDateRetour();
-        var duree = demande.getDuree();
-        if(Period.between(date_deb,date_retour).getDays()<duree)
-            throw new EntityException("Durée du congé invalide",400);
+        short duree = (short)demande.getDuree();
+        DateFinRetourDTO dateFinRetour = extractDateFromDebut(demande.getDateDebut(), duree);
+
+        if(!dateFinRetour.dateFin().equals(demande.getDateFin()) || !dateFinRetour.dateRetour().equals(demande.getDateRetour()))
+            throw new EntityException("Date fin et/ou retour invalide",400);
 
         // Check if he has enough balance
         String cin = demande.getPersonCin();
@@ -96,20 +129,9 @@ public class CongeService {
             throw new EntityException("You have used all of your balance",400);
 
         // Modifying the balance
-        var iterator = soldeListe.listIterator();
-        while(iterator.hasNext() && duree!=0){
-            SoldeConge element = iterator.next();
-            if(duree-element.getSoldeRestant()>0){
-               duree -= element.getSoldeRestant();
-               element.setSoldeRestant(0);
-            }
-            else{
-                element.setSoldeRestant(element.getSoldeRestant()-duree);
-                duree=0;
-            }
-        }
+        duree = updateBalance(soldeListe, duree, demande.getTypeId()==0);
         if(duree>0)
-            throw new EntityException("You have used all of your balance",400);
+            throw new EntityException("Not enough balance",400);
 
         // Accepting the demand
         demande.setStatusConge(DemandeConge.DEMANDE_ACCEPTED);
@@ -118,9 +140,55 @@ public class CongeService {
         conge.persist();
 
         // notify user
-        Notification notif = new Notification("Demande congé accepté","Your demande id="+demande_id+" was accepted", "success", demande.getPersonCin());
+        Notification notif = new Notification("Demande congé accepté","Your demande id="+demande_id+" was accepted", "info", demande.getPersonCin());
         notificationService.sendMsg(notif);
     }
+
+
+    /* I need something that, when given dateDebut + duree, it will return dateFin && dateRetour, taking into account the jours feriers*/
+    private Set<String> getJoursFeriers(){
+        return JoursFeriers.find("SELECT j.id.day, j.id.month, COALESCE(NULLIF(j.id.year, '--'), '2025') FROM JoursFeriers j").project(JoursFeriersDTO.class)
+                .stream()
+                .map(jf-> JoursFeriers.generateDateString(jf.day(),jf.month(), jf.year()))
+                .collect(Collectors.toSet());
+    }
+
+    public DateFinRetourDTO extractDateFromDebut(DateDebutDureeDTO dateDebutDuree){
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        try{
+            LocalDate currentDate = LocalDate.parse(dateDebutDuree.dateDebut()/*, formatter*/);
+            LocalDate dateFin=null, dateRetour=null;
+            Set<String> joursFeriers = getJoursFeriers();
+            short joursParcourus = 0;
+
+            while(joursParcourus<=dateDebutDuree.duree()){
+                if(currentDate.getDayOfWeek()== DayOfWeek.SUNDAY){
+                    currentDate = currentDate.plusDays(1);
+                }
+                if(joursFeriers.contains(JoursFeriers.generateDateString(currentDate))){
+                    currentDate = currentDate.plusDays(1);
+                    continue;
+                }
+                if(joursParcourus== dateDebutDuree.duree()-1){
+                    // This is the last day AKA dateFin
+                    dateFin = currentDate;
+                }
+
+                if(joursParcourus == dateDebutDuree.duree()){
+                    dateRetour = currentDate;
+                    break;
+                }
+                currentDate = currentDate.plusDays(1);
+                joursParcourus++;
+            }
+
+            return new DateFinRetourDTO(dateFin, dateRetour);
+        }catch (DateTimeParseException e){
+            e.printStackTrace();
+            throw new EntityException("Given date not in the 'dd/MM/yyyy' format", 400);
+        }
+    }
+
 
     public List<DemandeConge> getDemandes() {
         return demandeCongeRepository.listAll();
@@ -144,6 +212,47 @@ public class CongeService {
     }
 
     public void ajouterSoldeConge(DemandeAjoutSoldeDTO dto){
-        congeMapper.dtoToDemandeAjout(dto);
+        DemandeAjoutSolde demandeAjoutSolde = congeMapper.dtoToDemandeAjout(dto);
+        //TODO: complete this!
     }
+
+    public void addJoursFeriers(JoursFeriersDTO joursFeriersDTO) {
+        JoursFeriers joursFeriers = new JoursFeriers();
+        JoursFeriersId id = new JoursFeriersId();
+        id.day = joursFeriersDTO.day();
+        id.month = joursFeriersDTO.month();
+        id.year = joursFeriersDTO.year();
+        joursFeriers.setId(id);
+        joursFeriers.persist();
+    }
+
+    private DateFinRetourDTO extractDateFromDebut(LocalDate currentDate, short duree){
+            LocalDate dateFin=null, dateRetour=null;
+            Set<String> joursFeriers = getJoursFeriers();
+            short joursParcourus = 0;
+
+            while(joursParcourus<=duree){
+                if(currentDate.getDayOfWeek()== DayOfWeek.SUNDAY){
+                    currentDate = currentDate.plusDays(1);
+                }
+                if(joursFeriers.contains(JoursFeriers.generateDateString(currentDate))){
+                    currentDate = currentDate.plusDays(1);
+                    continue;
+                }
+                if(joursParcourus== duree-1){
+                    // This is the last day AKA dateFin
+                    dateFin = currentDate;
+                }
+
+                if(joursParcourus == duree){
+                    dateRetour = currentDate;
+                    break;
+                }
+                currentDate = currentDate.plusDays(1);
+                joursParcourus++;
+            }
+
+            return new DateFinRetourDTO(dateFin, dateRetour);
+    }
+
 }
