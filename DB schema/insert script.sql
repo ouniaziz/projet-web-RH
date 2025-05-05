41b8057f4fe60c45c050f387b41eb4cdc67560d5
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-05 06:32:15 +0100

    Added another insert query

--- DB schema/insert script.sql
+++ DB schema/insert script.sql
@@ -31,3 +31,21 @@ VALUES
     ('MATH', 'Mathématiques'),
     ('PHYS', 'Physique'),
     ('ELEC', 'Electronique'),
+
+
+INSERT INTO JOURS_FERIES (day, month, year) VALUES
+-- Fixed holidays (recurring every year)
+    (1, 1, '--'),       -- Nouvel An
+    (20, 3, '--'),      -- Fête de l'Indépendance
+    (9, 4, '--'),       -- Journée des Martyrs
+    (1, 5, '--'),       -- Fête du Travail
+    (25, 7, '--'),      -- Fête de la République
+    (13, 8, '--'),      -- Fête de la Femme
+    (15, 10, '--'),     -- Fête de l'Évacuation
+    (17, 12, '--'),     -- Fête de la Révolution
+
+-- Islamic holidays (specific dates for 2025)
+    (30, 3, 2025),      -- Aïd al-Fitr (2/3 jours, dépend du secteur d’activité)
+    (6, 6, 2025),       -- Aïd al-Adha (2 jours)
+    (26, 6, 2025),      -- Jour de l’An Hégire (Ras El Am El Hijri)
+    (4, 9, 2025);       -- Al-Mawlid Al-Nabawi (Mouled)
