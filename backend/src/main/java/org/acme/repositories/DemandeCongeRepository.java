ea815db7d1c66a6453d97aae413fea6f6820ac6c
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-06 19:24:17 +0100

    Added methods for @GET endpoint to fetch demandeConge by CIN

--- backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
+++ backend/src/main/java/org/acme/repositories/DemandeCongeRepository.java
@@ -2,8 +2,11 @@ package org.acme.repositories;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.dto.conge.DemandeCongeDTO;
+import org.acme.dto.response.CongeDTO;
 import org.acme.entities.conge.DemandeConge;
 
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -18,4 +21,16 @@ public class DemandeCongeRepository implements PanacheRepository<DemandeConge> {
                 WHERE d.id= ?1
                 """, id).singleResultOptional();
     }
+
+    public List<DemandeConge> findByCinOptimized(String cin){
+        return find("""
+                SELECT d
+                FROM DemandeConge d
+                LEFT JOIN FETCH d.person
+                LEFT JOIN FETCH d.exercice
+                LEFT JOIN FETCH d.type
+                WHERE d.person.cin = ?1
+                """, cin).list();
+    }
+
 }
