e5ce9dc6c3ff84e2188d15508c87277a7ceb2c8c
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-05-06 19:10:33 +0100

    Made routes role-dependant Added enseignant routes Added employeRoutes Modified current integral routes to be superRoutes Changed components to check the user's role before showing routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "layouts/MainLayout/MainLayout";
 import Page_connexion from "features/Auth/views/Page_connexion";
-import routes from "routes";
+import {employeRoutes, enseignantRoutes, superRoutes} from "routes";
 import ProtectedRoute from "./ProtectedRoute";
 import Page_reset from "features/reset_password/views/Page_reset";
 import ActivateAccount from "features/ActivateAccount/views/Page_activation";
 import {NotistackProvider} from "./components/NotificationProvider";
+import {useStore} from "./service/store";
 
 
 const getRoutes = (allRoutes) =>
   allRoutes.map((route) => <Route key={route.key} path={route.route} element={route.component} />);
 
 export default function App() {
-  return (
+    const role = useStore(state=>state.role);
+    return (
         <NotistackProvider>
             <Routes>
                   <Route path="/" element={<Page_connexion />} />
@@ -24,14 +26,14 @@ export default function App() {
 
                             <MainLayout />
                     }>
-                      {getRoutes(routes)}
+                      {getRoutes((role==="Administrateur" || role==="Personnel RH")? superRoutes:(role==="Enseignant")? enseignantRoutes: employeRoutes)}
                     </Route>
                   </Route>
               {/* <Route path="*" element={<Navigate to="/" />} /> */}
 
             </Routes>
         </NotistackProvider>
-  );
+    );
 }
 // {
 //   type: "collapse",
